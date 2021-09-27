27da527959f01a89c767497e2eacc54ec488f53d
Alexander Apraksin <72949586+Nuu44a@users.noreply.github.com>
2021-09-27 17:20:56 +0300

    Changed input command on server
    For now on server command selecting from given choice

--- test_server.py
+++ test_server.py
@@ -1,61 +1,78 @@
 import asyncio
+import struct
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 9999
 
-COMMAND_LIST = ['0x12', '0x13', '0x20']
 HELLO_MESSAGE = '''
-    Send command to lantern in HEX.
+    Send command to lantern.
     Avaliable commands:
-    0x00 - Disconnect
-    0x12 - Turn light ON
-    0x13 - Turn light OFF
-    0x20 - Change COLOR of the light
-    '''
+    1 - Turn light ON
+    2 - Change COLOR of the light
+    3 - Unavailable command
+    0 - Turn light OFF
+'''
+
+
+def command_on():
+    # Команда на включение фонаря
+    return struct.pack('>bh', 0x12, 0)
+
+def command_off():
+    # Команда на выключение фонаря
+    return struct.pack('>bh', 0x13, 0)
+
+def command_error():
+    # Несуществующая команда
+    return struct.pack('>bh', 0x14, 0)
+
+def command_color():
+    # Изменение цвета фонаря
+    print('Input color in format "#RRGGBB" (00-FF)')
+    color = input().lstrip('#')
+    r, g, b = [int(color[i:i+2], 16) for i in [0, 2, 4]]
+    # bytearr = struct.pack('>bhbbb', 0x20, 0, r-128, g-128, b-128)
+    return struct.pack('>bhbbb', 0x20, 3, r-128, g-128, b-128)
+
 
 async def handle_echo(reader, writer):
-    '''
-        Функция обслуживания клиента
-    '''
-    command_to_client = '0x00'
-    # Ждем клиента, читаем его статус
-    data = await reader.read()
+    data = await reader.read(10)
     status = data.decode()
-    # Получаем адрес клиента
-    await writer.drain()
     addr = writer.get_extra_info('peername')
-    # Выводим сообщение о соединении и статус клиента
     print(f'Esteblished conection with {addr!r}, status is {status!r}')
     
     # В этом цикле отправляем команды клиенту
-    while command_to_client:
+    while True:
         # Выводим краткую инструкцию и получаем команду для клиента
         tlv = ''
-        print(HELLO_MESSAGE)
-        command_to_client = input('Write command to Client -> ')
         
-        # Если команда существует, то обрабатываем ее
-        if command_to_client in COMMAND_LIST:
-            # Формируем TLV-строку для отправки клиенту
-            if command_to_client == '0x20':
-            
-            else:
-                tlv += hex(int(command_to_client, 16))
-                
-            print(f'Send: {command_to_client!r}')
-            writer.write(command_to_client.encode())
-            await writer.drain()
-            
-            # Получаем подтверждение от клиента
-            data = await reader.read()
-            message = data.decode()
-            print(f'Command recieved, new status is {status!r}')
-        # Команда на отключение от клиента
-        elif command_to_client == '0x00':
-            command_to_client = ''
-        # Проверка на дурака и повторяем цикл
+        print(HELLO_MESSAGE)
+        command_to_client = input('Choose command to Client -> ')
+        
+        # Формируем TLV-строку для отправки клиенту
+        if command_to_client == '1':
+            tlv = command_on()
+        elif command_to_client == '2':
+            tlv = command_color()
+        elif command_to_client == '3':
+            tlv = command_error()
+        elif command_to_client == '0':
+            tlv = command_off()
         else:
-            print('Unsupported command!')
+            print('Wrong command. Try again.')
+            continue
+            
+        print(f'Send: {tlv!r}')
+        writer.write(tlv)
+        await writer.drain()
+
+        # Получаем подтверждение от клиента
+        data = await reader.read(10)
+        status = data
+        print(f'Command recieved, new status is {status!r}')
+        
+        if command_to_client == '0':
+            break
         
     # Прощаемся и закрываем соединение
     print('Close the connection')
@@ -70,6 +87,9 @@ async def main():
     
     async with server:
         await server.serve_forever()
-
+    
+# Close the server
+    server.close()
+    print('Server SHUTDOWN')
 
 asyncio.run(main())
